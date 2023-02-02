c7c901fda91c30d6aa803af6b8bb647dcc539780
Bminusldk <51542263+Bminusldk@users.noreply.github.com>
2023-02-02 06:09:41 -0800

    Create mysql_test.py

---
+++ module_6/mysql_test.py
@@ -0,0 +1,25 @@
+import mysql.connector
+    from mysql-connector-python import errorcode
+    config = {
+        "user": "movies_user",
+        "password": "popcorn",
+        "host": "127.0.0.1",
+        "database": "movies",
+        "raise_on_warnings": True
+    }
+try:
+    db + mysql.connector.connect(**config)
+    print(*\n Database user {} connected to MySQL on host {}*.format(config[*user*], config[*host*], config[*database*]))
+    input(*\n\n Press any key to continue. . .*)
+except mysql.connector.Error as err:
+	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print(*   The suppied username or password are invalid*)
+        
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(* The specified database does not exist*)
+        
+    else:
+        print(err)
+        
+    finally:
+        db.close()
